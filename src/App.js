c64d8c289bb09776b91773d49fe4af5fe7292123
Daniela Carey <danielafcarey@gmail.com>
2018-06-14 15:24:22 -0600

    Update aggregate components with total playlist and hours

--- src/App.js
+++ src/App.js
@@ -1,11 +1,80 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class Aggregate extends Component {
+let fakeServerData = {
+  user: {
+    name: 'Daniela',
+    playlists: [
+      {
+        name: 'Playlist1',
+        songs: [
+          { name: 'song1',
+            duration: 1134
+          }, 
+          { name: 'song2',
+            duration: 2134,
+          } 
+        ]
+      },
+      {
+        name: 'Playlist2',
+        songs: [
+          { name: 'song1',
+            duration: 1134
+          }, 
+          { name: 'song2',
+            duration: 2134,
+          } 
+        ]
+      },
+      {
+        name: 'Playlist3',
+        songs: [
+          { name: 'song1',
+            duration: 112
+          }, 
+          { name: 'song2',
+            duration: 2124,
+          } 
+        ]
+      },
+      {
+        name: 'Playlist4',
+        songs: [
+          { name: 'song1',
+            duration: 1235
+          }, 
+          { name: 'song2',
+            duration: 235134,
+          } 
+        ]
+      },
+    ]
+  }
+}
+
+class PlaylistCounter extends Component {
   render () {
     return (
       <div className='Aggregate'>
-        <h2>Number Text</h2> 
+        <h2>{ this.props.playlists && this.props.playlists.length } Playlists</h2> 
+      </div>
+    )
+  }
+}
+
+class HoursCounter extends Component {
+  render () {
+    let allSongs = this.props.playlists.reduce((allSongs, playlist) => {
+      allSongs = [...allSongs, ...playlist.songs] 
+      return allSongs;
+    }, [])
+    let totalDuration = allSongs.reduce((totalDuration, song) => {
+      return totalDuration + song.duration;
+    }, 0);
+    return (
+      <div className='Aggregate'>
+        <h2>{ this.props.playlists && Math.floor(totalDuration/3600) } Hours</h2> 
       </div>
     )
   }
@@ -38,12 +107,35 @@ class Playlist extends Component {
 }
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      serverData: {}
+    }
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ serverData: fakeServerData })
+    }, 1000)
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>Better Playlists</h1>
-        <Aggregate />
-        <Aggregate />
+        {
+          this.state.serverData.user ? 
+          <div>
+            <h1>{ this.state.serverData.user.name }'s Playlists </h1>
+            <PlaylistCounter 
+              playlists={ this.state.serverData.user.playlists }
+            />
+            <HoursCounter 
+              playlists={ this.state.serverData.user.playlists }
+            />
+          </div> :
+            <h1>Loading...</h1>
+        }
         <Filter />
         <Playlist />
         <Playlist />
